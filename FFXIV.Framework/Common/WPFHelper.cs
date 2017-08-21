725b1197f815fc48ec06792209c9f3fe53f72ea2
anoyetta <anoyetta@gmail.com>
2017-08-22 00:42:07 +0900

    モダン版フォント、カラーダイアログを追加した

--- FFXIV.Framework/Common/WPFHelper.cs
+++ FFXIV.Framework/Common/WPFHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Threading;
 
@@ -22,6 +24,37 @@ namespace FFXIV.Framework.Common
             Application.Current?.Dispatcher.Invoke(
                 action,
                 priority);
+        }
+
+        private static int _IsDebugMode = -1;
+
+        /// <summary>
+        /// 現在のプロセスがデザインモードかどうか返します。
+        /// </summary>
+        public static bool IsDesignMode
+        {
+            get
+            {
+                if (_IsDebugMode == -1)
+                {
+                    if (LicenseManager.UsageMode == LicenseUsageMode.Designtime)
+                    {
+                        _IsDebugMode = 1;
+                    }
+                    else
+                    {
+                        using (var p = Process.GetCurrentProcess())
+                        {
+                            _IsDebugMode = (
+                                p.ProcessName.Equals("DEVENV", StringComparison.OrdinalIgnoreCase) ||
+                                p.ProcessName.Equals("XDesProc", StringComparison.OrdinalIgnoreCase)
+                            ) ? 1 : 0;
+                        }
+                    }
+                }
+
+                return _IsDebugMode == 1;
+            }
         }
     }
 }
